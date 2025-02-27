62141f5ad1e74b57cdcac8342ed664b4d5bf6496
Idealist <2046613303@qq.com>
2025-02-27 16:22:28 +0800

    Method overload exercises one

---
+++ java/src/Add.java
@@ -0,0 +1,19 @@
+public class Add {
+    int a;
+    int b;
+    double c;
+    double d;
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public double add(double c, double d) {
+        return c+d;
+    }
+    public double add(int a,double c) {
+        return a+c;
+    }
+    public double add(double c,int d) {
+        return c+d;
+    }
+
+}
--- java/src/Test.java
+++ java/src/Test.java
@@ -1,2 +1,25 @@
 public class Test {
+    static void added(int a, int b) {
+        System.out.println(a+b);
+    }
+    void added(double c, double d) {
+        System.out.println(c+d);
+    }
+    void added(int a,double c) {
+        System.out.println(a+c);
+    }
+    void added(double c,int d) {
+        System.out.println(c+d);
+    }
+    public static void main(String[] args) {
+
+                int num1=32;
+                double num2=6.66;
+                int num3=1;
+                Add add=new Add();
+                System.out.println(add.add(num1,num2));
+                System.out.println(add.add(num3,num2));
+                added(num3,num1);
+
+        }
 }

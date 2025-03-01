deefc1227d1533dba8696cac249648885029fa40
“Idealist” <2046613303@qq.com>
2025-03-02 03:01:45 +0800

    super practice one

--- java/src/Son.java
+++ java/src/Son.java
@@ -1,5 +1,10 @@
 public class Son extends Father {
     public Son() {
+        super(1);
         System.out.println("Son one");
+    }
+    public Son(int a) {
+//        super(1);
+        System.out.println("Son two");
     }
 }
--- java/src/TestExtend.java
+++ java/src/TestExtend.java
@@ -1,5 +1,6 @@
 public class TestExtend {
     public static void main(String[] args) {
         Son son = new Son();
+        Son son1 = new Son(1);
     }
 }
